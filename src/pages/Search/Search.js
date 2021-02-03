ed22c3f3ca19d9977b2da90b79ab5b7ebb5abda1
unknown <ethan.sayagh@gmail.com>
2021-02-03 16:03:16 +0200

    first commit

---
+++ src/pages/Search/Search.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Search.css";
+
+const Search = () => {
+  return <h1>SearchPage</h1>;
+};
+
+export default Search;
---
+++ src/pages/Term/Term.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Term.css";
+
+const Term = () => {
+  return <h1>TermPage</h1>;
+};
+
+export default Term;

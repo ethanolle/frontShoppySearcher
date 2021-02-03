ffaa623c6c1332faafc3a426eb59abcbf6e5aa4c
unknown <ethan.sayagh@gmail.com>
2021-02-03 16:20:45 +0200

    advanced base structure

---
+++ src/components/ResultsGrid/ResultsGrid.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResultGrid = () => {
+  return <h1>ResultGrid</h1>;
+};
+
+export default ResultGrid;
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,8 +1,15 @@
 import React from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import ResultGrid from "../../components/ResultsGrid/ResultsGrid";
 import "./Search.css";
 
 const Search = () => {
-  return <h1>SearchPage</h1>;
+  return (
+    <div>
+      <SearchBar />
+      <ResultGrid />
+    </div>
+  );
 };
 
 export default Search;

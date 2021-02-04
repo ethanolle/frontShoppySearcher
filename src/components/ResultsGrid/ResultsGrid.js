088b652127f5a166a58e8b3269549ae518871ab9
unknown <ethan.sayagh@gmail.com>
2021-02-04 21:43:23 +0200

    added option to opanall links

--- src/components/ResultsGrid/ResultsGrid.js
+++ src/components/ResultsGrid/ResultsGrid.js
@@ -1,10 +1,24 @@
 import React from "react";
 
 const ResultGrid = ({ resultGrid = [] }) => {
+  const openInNewTab = (url) => {
+    console.log("inFunction");
+    var win = window.open(url, "_blank");
+  };
+  const handleOpenAll = () => {
+    resultGrid.forEach((result) => {
+      openInNewTab(result);
+    });
+  };
   return (
     <div className='result'>
+      <button onClick={handleOpenAll}>Open All</button>
       {resultGrid.map((result) => (
-        <div className='result'>{result}</div>
+        <div className='result'>
+          <a href={result} target='_blank'>
+            {result}
+          </a>
+        </div>
       ))}
     </div>
   );

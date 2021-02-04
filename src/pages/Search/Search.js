3b99906e639643e8ecc530cc4199248f4bb227f0
unknown <ethan.sayagh@gmail.com>
2021-02-04 12:09:56 +0200

    added structure for request

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,27 +1,45 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ResultGrid from "../../components/ResultsGrid/ResultsGrid";
 import "./Search.css";
 
 const Search = (props) => {
   const [input, setInput] = useState("");
+  const [response, setResponse] = useState("");
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     alert(`${input}`);
+    setResponse(getDataFromBackend());
+  };
+
+  const getDataFromBackend = (product) => {
+    // axios({
+    //   method: "post",
+    //   url: "/login",
+    //   data: {
+    //     product: product,
+    //   },
+    // });
+    return "test";
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Enter Product
-        <input
-          type='text'
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-      </label>
-      <input type='submit' value='Submit' />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter Product
+          <input
+            type='text'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+      <h1>{response}</h1>
+      <ResultGrid />
+    </div>
   );
 };
 

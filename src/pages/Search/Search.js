711d54315d55bb0d5218a574c28d6186714ef035
unknown <ethan.sayagh@gmail.com>
2021-02-04 11:17:12 +0200

    did input + submit button

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,39 +1,27 @@
 import React, { useState, useEffect } from "react";
-import SearchBar from "../../components/SearchBar/SearchBar";
 import ResultGrid from "../../components/ResultsGrid/ResultsGrid";
 import "./Search.css";
 
 const Search = (props) => {
   const [input, setInput] = useState("");
-  const [countryListDefault, setCountryListDefault] = useState();
-  const [countryList, setCountryList] = useState();
 
-  const fetchData = async () => {
-    return await fetch("https://restcountries.eu/rest/v2/all")
-      .then((response) => response.json())
-      .then((data) => {
-        setCountryList(data);
-        setCountryListDefault(data);
-      });
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert(`${input}`);
   };
 
-  const updateInput = async (input) => {
-    const filtered = countryListDefault.filter((country) => {
-      return country.name.toLowerCase().includes(input.toLowerCase());
-    });
-    setInput(input);
-    setCountryList(filtered);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
   return (
-    <>
-      <h1>Country List</h1>
-      <SearchBar input={input} onChange={updateInput} />
-      <ResultGrid resultGrid={countryList} />
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Enter Product
+        <input
+          type='text'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </label>
+      <input type='submit' value='Submit' />
+    </form>
   );
 };
 

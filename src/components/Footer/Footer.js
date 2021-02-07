fd24e3cf41eeab0530704446f9a05364cbe28aab
ethan sayagh <ethan.sayagh@gmail.com>
2021-02-07 20:50:25 +0200

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,12 @@
-import React from "react";
-
+import React from 'react';
+import './Footer.css';
 const Footer = () => {
-  return <h1>Im the Footer</h1>;
+  return (
+    <div className='footer'>
+      <h3>Discord: ethanolle#4089</h3>
+      <h3>Skype: Ethanolle</h3>
+    </div>
+  );
 };
 
 export default Footer;
